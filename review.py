157e73d6a3cb416988a9aed744e942cd3e164742
tvivian12 <tanman.vivian@gmail.com>
2026-02-26 13:23:06 +1300

    Complete Day 2 Fundamentals

---
+++ review.py
@@ -0,0 +1,10 @@
+age=int(input("Enter your age: "))
+if age >=18:
+    print("You are an adult")
+else:print("You are a minor")
+for i in range(1,5):
+    print("Counting:", i)
+count = 0
+while count < 5:
+    print("While loop:", count)
+    count += 1
