1d7869048693dbcaa15902b029b684e768b3b9d7
tvivian12 <tanman.vivian@gmail.com>
2026-03-04 19:13:21 +1300

    Employee profile generator practice

---
+++ employee_profile.py
@@ -0,0 +1,17 @@
+name = 'Atticus Finch'
+age = 11 # years old
+
+experience_years = 10 # years of being the best son in the world
+
+department = 'Finch Family'
+position = 'Son'    
+
+
+employee_profile = f' Name: {name} | Age: {age} | Experience: {experience_years} years of being the best boy in the world | Department: {department} | Position: {position}'
+print(employee_profile)
+
+employee_card = 'DOG-2026-AF-069'
+print('Employee Card Number:', employee_card)   
+
+employee_code = employee_card[0:3] + employee_card[8:11 ] + employee_card[4:8]
+print('Employee Code:', employee_code)  
